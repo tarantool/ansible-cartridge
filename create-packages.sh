e3b95aced59a1f9e55e91e5b21c75a32dc4e08ed
Elizaveta Dokshina <eldokshina@mail.ru>
2019-10-31 22:21:41 +0300

    Separate Dockerfile
    Use separated Dockerfile for opensource and enterprise tarantool
    Improve create-packages.sh to pass right build args

--- create-packages.sh
+++ create-packages.sh
@@ -6,6 +6,20 @@ mkdir packages || true
 rm packages/* || true
 
 tarantool_version=${TARANTOOL_VERSION:-opensource-1.10}
+dockerfile_suffix=opensource
+
+if [[ $tarantool_version == "enterprise" ]] ;
+then
+    dockerfile_suffix=enterprise
+fi
+
+if [[ $tarantool_version == "opensource-2.2" ]] ;
+then
+    tarantool_repo=tarantool/2_2
+elif [[ $tarantool_version == "opensource-1.10" ]] ;
+then
+    tarantool_repo=tarantool/1_10
+fi
 
 IMAGE=${tarantool_version}-packages-builder
 CONTAINER=${IMAGE}-container
@@ -15,9 +29,12 @@ echo "Build packages for ${tarantool_version}"
 docker rm ${CONTAINER} || true
 
 docker build --build-arg TARANTOOL_DOWNLOAD_TOKEN=${TARANTOOL_DOWNLOAD_TOKEN} \
-                --build-arg BUNDLE_VERSION=${BUNDLE_VERSION} \
-                -t ${IMAGE}\
-                --target ${IMAGE} .
+             --build-arg BUNDLE_VERSION=${BUNDLE_VERSION} \
+             --build-arg TARANTOOL_REPO=${tarantool_repo} \
+             -t ${IMAGE} \
+             -f Dockerfile.${dockerfile_suffix} \
+             -t ${IMAGE} .
+
 docker create --name ${CONTAINER} ${IMAGE} usr/bin/true
 docker cp ${CONTAINER}:/opt/myapp/myapp-1.0.0-0.rpm .
 docker cp ${CONTAINER}:/opt/myapp/myapp-1.0.0-0.deb .
