23b84b62ea7a1120f00870089ec9b8365cc7c011
Elizaveta Dokshina <32142520+dokshina@users.noreply.github.com>
2019-11-20 18:18:39 +0300

    Manage memtx_memory in runtime (#41)
    Try to increase memtx_memory without instance restart (using
    `cartridge_instance` module)
    Add `cartridge_needs_restart` module to check if instance restart is
    required
    Add `restarted` variable to specify that instance must be restarted
    Describe changes in README
    * Update CHANGELOG

---
+++ library/cartridge_instance.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.helpers import ModuleRes, CartridgeException
+from ansible.module_utils.helpers import get_control_console
+
+import os
+
+
+argument_spec = {
+    'control_sock': {'required': True, 'type': 'str'},
+    'config': {'required': True, 'type': 'dict'},
+    'cartridge_defaults': {'required': True, 'type': 'dict'},
+}
+
+
+def manage_instance(params):
+    config = params['config']
+    cartridge_defaults = params['cartridge_defaults']
+    control_sock = params['control_sock']
+
+    # Check if memtx_memory parameter is specified
+    if 'memtx_memory' not in config and 'memtx_memory' not in cartridge_defaults:
+        return ModuleRes(success=True, changed=False)
+
+    new_memtx_memory = None
+    if 'memtx_memory' in config:
+        new_memtx_memory = config['memtx_memory']
+    else:
+        new_memtx_memory = cartridge_defaults['memtx_memory']
+
+    # Check if instance is started
+    if not os.path.exists(control_sock):
+        return ModuleRes(success=True, changed=False)
+
+    control_console = get_control_console(control_sock)
+
+    # Get current memtx memory
+    current_memtx_memory = control_console.eval('''
+        return type(box.cfg) ~= 'function' and box.cfg.memtx_memory or require('json').NULL
+    ''')
+    if current_memtx_memory is None:
+        # box.cfg wasn't called
+        return ModuleRes(success=True, changed=False)
+
+    if new_memtx_memory <= current_memtx_memory:
+        return ModuleRes(success=True, changed=False)
+
+    # try to increase memtx_memory
+    increased = control_console.eval('''
+        local ok, err = pcall(function()
+            box.cfg {{ memtx_memory = {} }}
+        end)
+        if not ok then
+            if tostring(err):find("cannot decrease memory size at runtime") == nil then
+                error('failed to set memtx_memory: ' .. tostring(err))
+            end
+        end
+        return ok
+    '''.format(new_memtx_memory))
+
+    return ModuleRes(success=True, changed=increased)
+
+
+def main():
+    module = AnsibleModule(argument_spec=argument_spec)
+    try:
+        res = manage_instance(module.params)
+    except CartridgeException as e:
+        module.fail_json(msg=str(e))
+
+    if res.success is True:
+        module.exit_json(changed=res.changed, meta=res.meta)
+    else:
+        module.fail_json(msg=res.msg)
+
+
+if __name__ == '__main__':
+    main()
---
+++ library/cartridge_needs_restart.py
@@ -0,0 +1,153 @@
+#!/usr/bin/python
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.helpers import ModuleRes, CartridgeException
+from ansible.module_utils.helpers import get_control_console
+
+import os
+
+
+argument_spec = {
+    'restart_forced': {'required': True, 'type': 'bool'},
+    'control_sock': {'required': True, 'type': 'str'},
+    'appname': {'required': True, 'type': 'str'},
+    'instance_name': {'required': True, 'type': 'str'},
+    'cluster_cookie': {'required': True, 'type': 'str'},
+    'cartridge_defaults': {'required': True, 'type': 'dict'},
+    'config': {'required': True, 'type': 'dict'},
+}
+
+
+def read_yaml_file_section(filepath, control_console, section):
+    sections = control_console.eval('''
+        local file = require('fio').open('{}')
+        if file == nil then
+            error('Failed to open instance config file')
+        end
+
+        local buf = {{}}
+        while true do
+            local val = file:read(1024)
+            if val == nil then
+                error('Failed to read from instance config file')
+            elseif val == '' then
+                break
+            end
+            table.insert(buf, val)
+        end
+        file:close()
+
+        local data = table.concat(buf, '')
+        local ok, ret = pcall(require('yaml').decode, data)
+        if not ok then
+            error('Failed to decode instance config from YAML')
+        end
+        return ret
+    '''.format(filepath))
+
+    if section not in sections:
+        raise CartridgeException('Instance file does not contain required section')
+
+    return sections[section]
+
+
+def check_conf_updated(new_conf, old_conf, ignore_keys=[]):
+    # check new conf keys
+    for key, value in new_conf.items():
+        if key not in ignore_keys:
+            if key not in old_conf or old_conf[key] != value:
+                return True
+
+    # check old conf keys
+    for key, value in old_conf.items():
+        if key not in ignore_keys:
+            if key not in new_conf or new_conf[key] != value:
+                return True
+
+    return False
+
+
+def get_memtx_memory(control_console):
+    return control_console.eval('''
+        return type(box.cfg) ~= 'function' and box.cfg.memtx_memory or require('json').NULL
+    ''')
+
+
+def needs_restart(params):
+    restart_forced = params['restart_forced']
+    if restart_forced:
+        return ModuleRes(success=True, changed=True)
+
+    control_sock = params['control_sock']
+    appname = params['appname']
+    instance_name = params['instance_name']
+    new_default_conf = params['cartridge_defaults']
+    new_instance_conf = params['config']
+    cluster_cookie = params['cluster_cookie']
+
+    instance_conf_path = '/etc/tarantool/conf.d/{}.{}.yml'.format(appname, instance_name)
+    default_conf_path = '/etc/tarantool/conf.d/{}.yml'.format(appname)
+    app_code_path = '/usr/share/tarantool/{}'.format(appname)
+
+    # check if instance was not started yet
+    if not os.path.exists(control_sock):
+        return ModuleRes(success=True, changed=True)
+
+    last_restart_time = os.path.getmtime(control_sock)
+
+    # check if application code was updated
+    package_update_time = os.path.getmtime(app_code_path)
+    if last_restart_time < package_update_time:
+        return ModuleRes(success=True, changed=True)
+
+    control_console = get_control_console(control_sock)
+
+    # check if instance config was changed (except memtx_memory)
+    current_instance_conf = read_yaml_file_section(
+        instance_conf_path,
+        control_console,
+        '{}.{}'.format(appname, instance_name)
+    )
+    if check_conf_updated(new_instance_conf, current_instance_conf, ['memtx_memory']):
+        return ModuleRes(success=True, changed=True)
+
+    # check if default config was changed (except memtx_memory)
+    current_default_conf = read_yaml_file_section(
+        default_conf_path,
+        control_console,
+        appname
+    )
+    new_default_conf.update({'cluster_cookie': cluster_cookie})
+    if check_conf_updated(new_default_conf, current_default_conf, ['memtx_memory']):
+        return ModuleRes(success=True, changed=True)
+
+    new_memtx_memory = None
+    if 'memtx_memory' in new_instance_conf:
+        new_memtx_memory = new_instance_conf['memtx_memory']
+    elif 'memtx_memory' in new_default_conf:
+        new_memtx_memory = new_default_conf['memtx_memory']
+
+    # check if memtx_memory was changed in config but wasn't changed on instance
+    if new_memtx_memory is not None:
+        current_memtx_memory = get_memtx_memory(control_console)
+        if current_memtx_memory != new_memtx_memory:
+            return ModuleRes(success=True, changed=True)
+
+    return ModuleRes(success=True, changed=False)
+
+
+def main():
+    module = AnsibleModule(argument_spec=argument_spec)
+    try:
+        res = needs_restart(module.params)
+    except CartridgeException as e:
+        module.fail_json(msg=str(e))
+
+    if res.success is True:
+        module.exit_json(changed=res.changed, meta=res.meta)
+    else:
+        module.fail_json(msg=res.msg)
+
+
+if __name__ == '__main__':
+    main()
