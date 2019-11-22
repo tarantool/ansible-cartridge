3249b915916fe19daaca6117787b0700f4cce2d2
Elizaveta Dokshina <eldokshina@mail.ru>
2019-11-22 12:24:09 +0300

    Wait for instance start with timeout
    Add `instance_start_timeout` parameter to specify time to wait for instance
    start
    Check if instance is started by socket: if is exists and connection is
    established, eval simple member status check

---
+++ library/cartridge_check_instance_started.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.helpers import ModuleRes, CartridgeException
+from ansible.module_utils.helpers import get_control_console
+
+
+argument_spec = {
+    'control_sock': {'required': True, 'type': 'str'},
+}
+
+
+def check_instance_started(params):
+    control_console = get_control_console(params['control_sock'])
+    ok = control_console.eval('''
+        return require('membership').myself().status == 'alive'
+    ''')
+
+    return ModuleRes(success=ok)
+
+
+def main():
+    module = AnsibleModule(argument_spec=argument_spec)
+
+    try:
+        res = check_instance_started(module.params)
+    except CartridgeException as e:
+        module.fail_json(msg=str(e))
+
+    if res.success is True:
+        module.exit_json(changed=res.changed, meta=res.meta)
+    else:
+        module.fail_json(msg=res.msg)
+
+
+if __name__ == '__main__':
+    main()
