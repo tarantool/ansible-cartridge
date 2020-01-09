26b98960af53e07e07d17e0dd2a7abd934d411f3
Elizaveta Dokshina <eldokshina@mail.ru>
2020-01-09 15:31:06 +0300

    Use ansible_host as a machine identifier
    Rewrite `one_not_expelled_instance_for_machine` filter to get machine
    identifier as `ansible_host`
    Use `inventory_hostname` if `ansible_host` is not defined

--- filter_plugins/filter_hosts.py
+++ filter_plugins/filter_hosts.py
@@ -2,11 +2,14 @@ def one_not_expelled_instance_for_machine(hostvars, play_hosts):
     res = []
     added_machines = {}
 
+    def get_machine_identifier(v):
+        return v['ansible_host'] if 'ansible_host' in v else v['inventory_hostname']
+
     for i in play_hosts:
         if 'expelled' in hostvars[i] and hostvars[i]['expelled']:
             continue
 
-        machine_id = hostvars[i]['ansible_machine_id']
+        machine_id = get_machine_identifier(hostvars[i])
         if machine_id not in added_machines:
             added_machines[machine_id] = True
             res.append(i)
