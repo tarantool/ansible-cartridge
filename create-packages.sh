aa829514167f8e39f2074e9321192fc0e9b42841
Elizaveta Dokshina <32142520+dokshina@users.noreply.github.com>
2020-01-23 16:59:48 +0300

    Store test packages in Gitlab CI cache (#70)

--- create-packages.sh
+++ create-packages.sh
@@ -2,11 +2,16 @@
 
 set -e
 
-mkdir packages || true
-rm packages/* || true
+packages_dirname="packages"
+
+if [ -d "${packages_dirname}" ]; then
+    echo "Directory packages already exists"
+    exit 0
+fi
 
 tarantool_version=${TARANTOOL_VERSION:-opensource-1.10}
 dockerfile_suffix=opensource
+cartridge_cli_version=${CARTRIDGE_CLI_VERSION:-1.2.1}
 
 if [[ $tarantool_version == "enterprise" ]] ;
 then
@@ -31,11 +36,17 @@ docker rm ${CONTAINER} || true
 docker build --build-arg TARANTOOL_DOWNLOAD_TOKEN=${TARANTOOL_DOWNLOAD_TOKEN} \
              --build-arg BUNDLE_VERSION=${BUNDLE_VERSION} \
              --build-arg TARANTOOL_REPO=${tarantool_repo} \
+             --build-arg CARTRIDGE_CLI_VERSION=${cartridge_cli_version} \
              -t ${IMAGE} \
              -f Dockerfile.${dockerfile_suffix} \
              -t ${IMAGE} .
 
 docker create --name ${CONTAINER} ${IMAGE} usr/bin/true
-docker cp ${CONTAINER}:/opt/myapp/myapp-1.0.0-0.rpm .
-docker cp ${CONTAINER}:/opt/myapp/myapp-1.0.0-0.deb .
+
+mkdir "${packages_dirname}"
+
+docker cp ${CONTAINER}:/opt/myapp/myapp-1.0.0-0.rpm ${packages_dirname}
+docker cp ${CONTAINER}:/opt/myapp/myapp-1.0.0-0.deb ${packages_dirname}
 docker rm ${CONTAINER}
+
+echo "Packages are placed in packages directory"
