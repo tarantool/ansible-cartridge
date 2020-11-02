bb08374b86203fe93db334f2ed638a1e4e9e1665
Roman Proskin <opomuc@gmail.com>
2020-11-02 14:41:29 +0300

    Support install under supervisord

--- filter_plugins/filter_hosts.py
+++ filter_plugins/filter_hosts.py
@@ -35,6 +35,14 @@ def get_one_not_expelled_instance_for_machine(hostvars, play_hosts):
     return res
 
 
+def get_instances_on_machine(hostvars, ansible_host):
+    res = []
+    for i in hostvars:
+        if i['ansible_host'] == ansible_host:
+            res.append(i)
+    return res
+
+
 def get_one_not_expelled_instance(hostvars, play_hosts):
     for i in play_hosts:
         if is_expelled(hostvars[i]) or is_stateboard(hostvars[i]):
@@ -99,4 +107,5 @@ class FilterModule(object):
             'get_instance_systemd_service': get_instance_systemd_service,
             'get_instance_run_dir': get_instance_run_dir,
             'is_stateboard_on_machine': is_stateboard_on_machine,
+            'get_instances_on_machine': get_instances_on_machine,
         }
