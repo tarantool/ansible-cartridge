1ec077098fb7b19e682273fa6b1262726b747710
Vladimir Severov <v.severov@corp.mail.ru>
2021-03-11 12:20:12 +0300

    Make probe asynchronous in batches

--- library/cartridge_connect_to_membership.py
+++ library/cartridge_connect_to_membership.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import math
 import pkgutil
 
 if pkgutil.find_loader('ansible.module_utils.helpers'):
@@ -13,15 +14,65 @@ argument_spec = {
     'play_hosts': {'required': True, 'type': 'list'},
 }
 
+BATCH_SIZE = 32
 
-def probe_server(control_console, uri):
-    return control_console.eval_res_err('''
-        local advertise_uri = ...
-        if require('membership').get_member(advertise_uri) ~= nil then
-            return false
+
+def connect_instances(control_console, play_hosts, instance_names, advertise_uris):
+    connect_errors, _ = control_console.eval_res_err('''
+        local advertise_uris = ...
+
+        local cartridge = require('cartridge')
+        local fiber = require('fiber')
+
+        local count = 0
+        local channel = fiber.channel()
+
+        for _, advertise_uri in ipairs(advertise_uris) do
+            count = count + 1
+
+            fiber.create(function()
+                local _, err = cartridge.admin_probe_server(advertise_uri)
+                channel:put({
+                    advertise_uri = advertise_uri,
+                    err = err and err.err or err,
+                })
+            end)
         end
-        return require('cartridge').admin_probe_server(advertise_uri)
-    ''', uri)
+
+        local errors = {}
+        for _ = 1, count do
+            local res = channel:get(60)
+            if res == nil then
+                table.insert(errors, {msg = 'Failed to get probe result: timeout'})
+                break
+            end
+            if res.err ~= nil then
+                table.insert(errors, {
+                    msg = ("Failed to probe server '%%s': %s"):format(res.err),
+                    advertise_uri = res.advertise_uri,
+                })
+            end
+        end
+        return errors
+    ''', advertise_uris)
+
+    errors = []
+    warnings = []
+
+    for error in connect_errors:
+        if not error['advertise_uri']:
+            errors.append(error['msg'])
+            break
+
+        instance_name = instance_names[error['advertise_uri']]
+        msg = error['msg'] % instance_name
+
+        if instance_name in play_hosts:
+            errors.append(msg)
+        else:
+            warnings.append(msg)
+
+    return errors, warnings
 
 
 def connect_to_membership(params):
@@ -29,7 +80,8 @@ def connect_to_membership(params):
     hostvars = params['hostvars']
     play_hosts = params['play_hosts']
 
-    changed = False
+    advertise_uris = []
+    instance_names = {}
 
     for instance_name, instance_vars in hostvars.items():
         if helpers.is_expelled(instance_vars) or helpers.is_stateboard(instance_vars):
@@ -38,14 +90,22 @@ def connect_to_membership(params):
         if 'config' not in instance_vars or 'advertise_uri' not in instance_vars['config']:
             continue
 
-        connected, err = probe_server(control_console, instance_vars['config']['advertise_uri'])
-        if err is not None and instance_name in play_hosts:
-            return helpers.ModuleRes(failed=True, msg=err)
+        advertise_uri = instance_vars['config']['advertise_uri']
+        advertise_uris.append(advertise_uri)
+        instance_names[advertise_uri] = instance_name
 
-        if connected:
-            changed = True
+    warnings = []
 
-    return helpers.ModuleRes(changed=changed)
+    for i in range(int(math.ceil(len(advertise_uris) / BATCH_SIZE))):
+        advertise_uris_batch = advertise_uris[i * BATCH_SIZE: (i + 1) * BATCH_SIZE]
+
+        errors, warns = connect_instances(control_console, play_hosts, instance_names, advertise_uris_batch)
+        warnings += warns
+
+        if len(errors) > 0:
+            return helpers.ModuleRes(failed=True, msg=errors, warnings=warnings)
+
+    return helpers.ModuleRes(changed=False, warnings=warnings)
 
 
 if __name__ == '__main__':
