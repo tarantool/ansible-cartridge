822def5051b90be5394978fe81694f348499ea7e
Vladimir Severov <v.severov@corp.mail.ru>
2021-05-27 18:21:43 +0300

    Add ZIP TDG configs support, CI for TDG
    Closes #293
    Closes #327
    
    Before the patch, it was possible to upload TDG config only in folder format. Also, there were no TDG tests in CI.
    
    Changes:
    - Now it's possible to upload the TDG config in ZIP format;
    - Added tests for TDG (including `config_upload_tdg` molecule test);
    - `config_upload` test fixed and added to CI matrix.

--- .github/helpers/count_molecule_matrix.py
+++ .github/helpers/count_molecule_matrix.py
@@ -31,23 +31,33 @@ def get_ee_params(molecule_scenario=None, ansible_version=None, sdk_version=None
     }
 
 
+def get_tdg_params(molecule_scenario=None, ansible_version=None, tdg_version=None, molecule_command=None):
+    return {
+        **get_matrix_base(molecule_scenario, ansible_version),
+        'tdg_version': tdg_version or '',
+        'molecule_command': get_molecule_command(molecule_command),
+    }
+
+
 def main(event_name, repo_owner, review_state, ref):
     ce_matrix = []
     ee_matrix = []
+    tdg_matrix = []
 
     if event_name == 'push' or event_name == 'pull_request' and repo_owner != 'tarantool':
         ce_matrix.append(get_ce_params())
 
     if event_name == 'workflow_dispatch' or review_state == 'approved' or ref == 'refs/heads/master':
-        ce_matrix.append(get_ce_params(molecule_scenario='tasks_from'))
-        ce_matrix.append(get_ce_params(molecule_scenario='update_cartridge'))
         ce_matrix.append(get_ce_params(molecule_scenario='check_facts'))
-        ce_matrix.append(get_ce_params(molecule_scenario='rolling_update'))
+        ce_matrix.append(get_ce_params(molecule_scenario='config_upload'))
+        ce_matrix.append(get_ce_params(molecule_scenario='dead_instances'))
+        ce_matrix.append(get_ce_params(molecule_scenario='eval'))
         ce_matrix.append(get_ce_params(molecule_scenario='needs_restart'))
         ce_matrix.append(get_ce_params(molecule_scenario='package_name'))
-        ce_matrix.append(get_ce_params(molecule_scenario='eval'))
+        ce_matrix.append(get_ce_params(molecule_scenario='rolling_update'))
         ce_matrix.append(get_ce_params(molecule_scenario='start_stop'))
-        ce_matrix.append(get_ce_params(molecule_scenario='dead_instances'))
+        ce_matrix.append(get_ce_params(molecule_scenario='tasks_from'))
+        ce_matrix.append(get_ce_params(molecule_scenario='update_cartridge'))
 
         ce_matrix.append(get_ce_params(tarantool_version='1.10'))
 
@@ -57,13 +67,31 @@ def main(event_name, repo_owner, review_state, ref):
         # TODO: Uncomment after fixing the check mode
         # ce_matrix.append(get_ce_version(molecule_command='check'))
 
+        # Tests with enterprise bundle
         ee_matrix.append(get_ee_params(sdk_version='2.6.2-124-g2c3b91439-r391'))
+
+        # TDG functionality: common.app_version [-]; admin.upload_config_api [-];
+        tdg_matrix.append(get_tdg_params(molecule_scenario='config_upload_tdg', tdg_version='1.6.10-4-g06ea889e'))
+        tdg_matrix.append(get_tdg_params(molecule_scenario='config_upload_tdg', tdg_version='1.7.1-0-g92920bea'))
+
+        # TDG functionality: common.app_version [+]; admin.upload_config_api [+];
+        tdg_matrix.append(get_tdg_params(molecule_scenario='config_upload_tdg', tdg_version='1.6.16-0-g7e140c94'))
+        tdg_matrix.append(get_tdg_params(molecule_scenario='config_upload_tdg', tdg_version='1.7.7-0-g76c31fca'))
 
     print(f'::set-output name=ce-tests-found::{"true" if len(ce_matrix) > 0 else "false"}')
     print(f'::set-output name=ee-tests-found::{"true" if len(ee_matrix) > 0 else "false"}')
+    print(f'::set-output name=tdg-tests-found::{"true" if len(tdg_matrix) > 0 else "false"}')
 
     print(f'::set-output name=ce-matrix::{json.dumps({"include": ce_matrix})}')
     print(f'::set-output name=ee-matrix::{json.dumps({"include": ee_matrix})}')
+    print(f'::set-output name=tdg-matrix::{json.dumps({"include": tdg_matrix})}')
+
+    print('Computed matrices:')
+    print(json.dumps({
+        'ce-matrix': ce_matrix,
+        'ee-matrix': ee_matrix,
+        'tdg-matrix': tdg_matrix,
+    }, indent=4))
 
 
 if __name__ == '__main__':
