8eec738fa776993841a6211a428ad750c96c909c
Vladimir Severov <v.severov@corp.mail.ru>
2021-05-27 18:18:52 +0300

    Update `create-packages.sh`
    Now it's possible to:
    - download TDG package;
    - skip Cartridge or TDG packages creating;
    - keep the existing directory;
    - select packages directory.

--- create-packages.sh
+++ create-packages.sh
@@ -6,77 +6,210 @@
 # Note, that if you are using OS X, you should have `gnu-sed` installed
 # and packing will be performed in docker in this case
 
-set -xe
+set -e
+
+#########
+# Flags #
+#########
 
 packages_dirname="packages"
+keep_old_packages=""
+skip_cartridge=""
+skip_tdg=""
+tdg_version="1.7.7-0-g76c31fca"
 
-if [ -d "${packages_dirname}" ]; then
-    echo "Remove existent packages directory"
-    rm -rf ${packages_dirname}
-fi
+usage() {
+    echo "Usage: ./create-packages.sh [options]"
+    echo "Options:"
+    echo "  -h, --help                              Show this help message"
+    echo "  -d <path>, --dir <path>                 Folder to save packages (default: ${packages_dirname})"
+    echo "  -k, --keep                              Don't delete packages folder (will be removed by default)"
+    echo "  --skip-cartridge                        Skip Cartridge packages creation (will be created by default)"
+    echo "  --skip-tdg                              Skip TDG packages creation (will be created by default)"
+    echo "  -t <version>, --tdg-version <version>   Version of TDG to download (default: ${tdg_version})"
+}
 
-pack_flags='--use-docker'
-
-if [[ $(tarantool -V) == "Tarantool Enterprise"* && -z "${TARANTOOL_SDK_PATH}" ]]; then
-    if [[ $(tarantool -V) == *"Target: Darwin"* ]]; then
-        echo "Set the path to Linux Tarantool SDK using the TARANTOOL_SDK_PATH environment variable!"
+while [ "$1" != "" ]; do
+    case "$1" in
+    -h | --help)
+        usage
+        exit
+        ;;
+    -d | --dir)
+        packages_dirname="$2"
+        shift
+        shift
+        ;;
+    -k | --keep)
+        keep_old_packages="true"
+        shift
+        ;;
+    --skip-cartridge)
+        skip_cartridge="true"
+        shift
+        ;;
+    --skip-tdg)
+        skip_tdg="true"
+        shift
+        ;;
+    -t | --tdg-version)
+        tdg_version="$2"
+        shift
+        shift
+        ;;
+    *)
+        echo "Unexpected parameter: ${1}"
         exit 1
-    else
-        pack_flags+=' --sdk-local'
+        ;;
+    esac
+done
+
+if [ -z "${skip_cartridge}" ]; then
+    pack_flags='--use-docker'
+    if [[ $(tarantool -V) == "Tarantool Enterprise"* && -z "${TARANTOOL_SDK_PATH}" ]]; then
+        if [[ $(tarantool -V) == *"Target: Darwin"* ]]; then
+            echo "Set the path to Linux Tarantool SDK using the TARANTOOL_SDK_PATH environment variable!"
+            exit 1
+        else
+            pack_flags+=' --sdk-local'
+        fi
     fi
 fi
 
-mkdir ${packages_dirname}
-pushd ${packages_dirname}
+###############
+# Preparation #
+###############
 
-# default test scenario
-appname=myapp
-version=1.0.0-0
+lazy_pack() {
+    lp_format="${1}"
+    lp_app_name="${2}"
+    lp_version="${3}"
 
-echo "Create packages for default test scenario"
+    lp_suffix="${4}"
+    if [ -n "${lp_suffix}" ]; then
+        lp_suffix_with_dash="-${lp_suffix}"
+    fi
 
-cartridge create --name ${appname}
+    lp_extension=${lp_format}
+    if [ "${lp_extension}" = "tgz" ]; then
+        lp_extension="tar.gz"
+    fi
 
-awk '{gsub(/cartridge.cfg\({/, "&\n    vshard_groups = { hot = { bucket_count = 20000 } },")}1' \
-    ${appname}/init.lua >${appname}/temp.lua
-mv ${appname}/temp.lua ${appname}/init.lua
+    package_name="${lp_app_name}-${lp_version}${lp_suffix_with_dash}.${lp_extension}"
+    if [ ! -f "${package_name}" ]; then
+        echo " - Package '${package_name}' creation:"
 
-awk '{gsub(/-- dependencies/, "dependencies")}1' \
-    ${appname}/app/roles/custom.lua >${appname}/temp.lua
-mv ${appname}/temp.lua ${appname}/app/roles/custom.lua
+        cartridge pack "${lp_format}" \
+            --version "${lp_version}" \
+            --suffix "${lp_suffix}" \
+            ${pack_flags} \
+            "${lp_app_name}"
+    else
+        echo " - Package '${package_name}' already created"
+    fi
+}
 
+if [ -d "${packages_dirname}" ] && [ -z "${keep_old_packages}" ]; then
+    printf "Removing an existing package directory... "
+    rm -rf "${packages_dirname}"
+    echo "OK"
+fi
 
-cartridge pack tgz --version ${version} ${pack_flags} ${appname}
-cartridge pack rpm --version ${version} ${pack_flags} ${appname}
-cartridge pack deb --version ${version} ${pack_flags} ${appname}
+mkdir -p "${packages_dirname}" >/dev/null
+pushd "${packages_dirname}" >/dev/null
 
-rm -rf ${appname}
+#######################################
+# Packages for default test scenarios #
+#######################################
 
-# update_cartridge test scenario
-appname=myapp
+if [ -z "${skip_cartridge}" ]; then
+    app_name=myapp
+    version=1.0.0-0
 
-echo "Create packages for update_cartridge test scenario"
+    echo ""
+    echo "Creating packages for default test scenarios:"
 
-cartridge create --name ${appname}
+    echo " - Application creation:"
+    rm -rf ${app_name}
+    cartridge create --name ${app_name}
 
-version=1  # myapp X version uses Cartridge 2.X.0 version
-for cartridge_version in '2.1.2' '2.2.0' '2.3.0' '2.4.0' '2.5.0'
-do
-    awk -v cartridge_dep_str="cartridge == ${cartridge_version}-1" \
-        '{gsub(/cartridge == [0-9.-]+/, cartridge_dep_str);}1' \
-        ${appname}/${appname}-scm-1.rockspec >${appname}/temp.rockspec
-    mv ${appname}/temp.rockspec ${appname}/${appname}-scm-1.rockspec
+    awk '{gsub(/cartridge.cfg\({/, "&\n    vshard_groups = { hot = { bucket_count = 20000 } },")}1' \
+        ${app_name}/init.lua >${app_name}/temp.lua
+    mv ${app_name}/temp.lua ${app_name}/init.lua
 
-    cartridge pack tgz \
-        --version ${version} \
-        --suffix with-c-${cartridge_version} \
-        ${pack_flags} \
-        ${appname}
+    awk '{gsub(/-- dependencies/, "dependencies")}1' \
+        ${app_name}/app/roles/custom.lua >${app_name}/temp.lua
+    mv ${app_name}/temp.lua ${app_name}/app/roles/custom.lua
 
-    ((version++))
-done
+    lazy_pack tgz "${app_name}" "${version}"
+    lazy_pack rpm "${app_name}" "${version}"
+    lazy_pack deb "${app_name}" "${version}"
 
-rm -rf ${appname}
+    rm -rf ${app_name}
+fi
 
-echo "Packages are placed in ${packages_dirname} directory"
-popd
+#################################################
+# Packages for 'update_cartridge' test scenario #
+#################################################
+
+if [ -z "${skip_cartridge}" ]; then
+    app_name=myapp
+
+    echo ""
+    echo "Creating packages for 'update_cartridge' test scenario:"
+
+    echo " - Application creation:"
+    rm -rf ${app_name}
+    cartridge create --name ${app_name}
+
+    version=1 # myapp X version uses Cartridge 2.X.0 version
+    for cartridge_version in '2.1.2' '2.2.0' '2.3.0' '2.4.0' '2.5.0'; do
+        awk -v cartridge_dep_str="cartridge == ${cartridge_version}-1" \
+            '{gsub(/cartridge == [0-9.-]+/, cartridge_dep_str);}1' \
+            ${app_name}/${app_name}-scm-1.rockspec >${app_name}/temp.rockspec
+        mv ${app_name}/temp.rockspec ${app_name}/${app_name}-scm-1.rockspec
+
+        lazy_pack tgz "${app_name}" "${version}.0.0-0" "with-c-${cartridge_version}"
+
+        ((version++))
+    done
+
+    rm -rf ${app_name}
+fi
+
+###################################
+# Packages for TDG test scenarios #
+###################################
+
+if [ -z "${skip_tdg}" ]; then
+    echo ""
+
+    if [ -f "tdg.tar.gz" ]; then
+        echo "TDG package 'tdg.tar.gz' already downloaded"
+    else
+        if [ -n "${DOWNLOAD_TNT_TOKEN}" ]; then
+            printf "Downloading TDG package from 'download.tarantool.io'... "
+            curl -L -s -o "tdg.tar.gz" \
+                "https://tarantool:${DOWNLOAD_TNT_TOKEN}@download.tarantool.io/tdg/tdg-${tdg_version}.tar.gz" >/dev/null
+            echo 'OK'
+        elif (command -v aws &>/dev/null) && [ -n "${AWS_ACCESS_KEY_ID}" ] && [ -n "${AWS_SECRET_ACCESS_KEY}" ]; then
+            printf "Downloading TDG package from MCS... "
+            aws --endpoint-url https://hb.bizmrg.com s3 cp \
+                "s3://packages/tdg/tdg-${tdg_version}.tar.gz" "tdg.tar.gz" >/dev/null
+            echo 'OK'
+        else
+            echo "[WARNING] Impossible to download TDG. It's necessary to run TDG tests. You can:"
+            echo " - specify 'DOWNLOAD_TNT_TOKEN' in environment to download from 'download.tarantool.io';"
+            echo " - install AWS CLI and specify 'AWS_ACCESS_KEY_ID' and 'AWS_SECRET_ACCESS_KEY' in environment to download from MCS;"
+            echo " - put 'tdg.tar.gz' package in '${packages_dirname}' directory manually."
+        fi
+    fi
+fi
+
+##############
+# Completion #
+##############
+
+echo ""
+popd >/dev/null
+echo "Packages are placed in '${packages_dirname}' directory"
